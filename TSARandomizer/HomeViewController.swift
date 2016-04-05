0df70771c230f7cec0b205128e66f4ee19ece684
alexpersian <alex_persian@me.com>
2016-04-05 09:01:13 -0500

    Adds third option, UP arrow

--- TSARandomizer/HomeViewController.swift
+++ TSARandomizer/HomeViewController.swift
@@ -12,6 +12,8 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var pressMeButton: UIButton!
     
+    var lastRand: Int = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,14 +24,31 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func random() -> Int {
+        return Int(arc4random_uniform(2))
+    }
 
     @IBAction func buttonPressed(sender: UIButton) {
-        let rand = Int(arc4random_uniform(2))
+        let rand = random()
+        
         switch rand {
-        case 0: performSegueWithIdentifier("leftViewControllerSegue", sender: self)
-        case 1: performSegueWithIdentifier("rightViewControllerSegue", sender: self)
+        case 0:
+            if rand == lastRand {
+                performSegueWithIdentifier("upViewControllerSegue", sender: self)
+            } else {
+                performSegueWithIdentifier("leftViewControllerSegue", sender: self)
+            }
+        case 1:
+            if rand == lastRand {
+                performSegueWithIdentifier("upViewControllerSegue", sender: self)
+            } else {
+                performSegueWithIdentifier("rightViewControllerSegue", sender: self)
+            }
         default: print("The world has broken")
         }
+        
+        lastRand = rand
     }
     
     @IBAction func returnToHomeViewController(segue: UIStoryboardSegue) {}
